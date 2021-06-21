de9e5317797e72cbc3cd9225d88208332c6dd8d6
Alexsandro G Bezerra <alexsandro.g.bezerra@gmail.com>
2021-06-21 19:17:36 -0300

    feat: Add signOut broadcast

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -5,11 +5,12 @@ import { AuthContext } from "../contexts/AuthContext"
 import { withSSRAuth } from "../utils/withSSRAuth"
 
 export default function Dashboard() {
-  const { user } = useContext(AuthContext)
+  const { user, signOut } = useContext(AuthContext)
 
   return (
     <>
       <h1>Dashboard: {user?.email}</h1>
+      <button type="button" onClick={signOut}>Sign out</button>
 
       <Can permissions={['metrics.list']}>
         <h1>Métricas</h1>
